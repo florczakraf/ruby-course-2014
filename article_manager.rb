1aed792264db7db9efd9ee8ca2ec4ff4f5325ae5
Rafal Florczak <florczak.raf@gmail.com>
2014-10-20 12:19:55 +0200

    [#5] Removed empty lines

--- article_manager.rb
+++ article_manager.rb
@@ -3,25 +3,22 @@ require_relative 'article'
 class ArticleManager
   attr_accessor :articles
 
-	def initialize
+  def initialize
     @articles = []
   end
   
   def best_articles
     check_size
-    
     @articles.sort { |art1, art2| art2.positive_votes <=> art1.positive_votes }
   end
 
   def worst_articles
     check_size
-    
     @articles.sort { |art1, art2| art1.positive_votes <=> art2.positive_votes }
   end
   
   def worst_article
     check_size
-    
     aux = @articles[0]
     @articles.each { |art| aux = art if art.positive_votes < aux.positive_votes }
     aux
@@ -29,7 +26,6 @@ class ArticleManager
   
   def best_article
     check_size
-    
     aux = @articles[0]
     @articles.each { |art| aux = art if art.positive_votes > aux.positive_votes }
     aux
@@ -37,7 +33,6 @@ class ArticleManager
   
   def most_popular_article
     check_size
-    
     aux = @articles[0]
     @articles.each { |art| aux = art if art.votes > aux.votes }
     aux
@@ -45,13 +40,11 @@ class ArticleManager
   
   def include?(pattern)
     check_size
-    
     @articles.find_all { |art| art.include? pattern }
   end
   
   def authors
     check_size
-    
     @articles.collect { |art| art.author }.uniq!
   end
   
@@ -61,13 +54,11 @@ class ArticleManager
 
   def votes
     check_size
-    
     @articles.inject(0) { |acc, art| acc + art.votes }
   end
   
   def to_s
-    check_size  
-    
+    check_size
     aux = "All articles:\n"
     @articles.each { |art| aux += art.to_s + "\n\n" }
     aux
