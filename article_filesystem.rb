b08d49554e2605c962e706dd5ab85b95288a82b6
Rafal Florczak <florczak.raf@gmail.com>
2014-10-27 13:35:32 +0100

    [#13] Added method to read article from file

--- article_filesystem.rb
+++ article_filesystem.rb
@@ -1,3 +1,5 @@
+require_relative 'article'
+
 class ArticleFilesystem
   ARTICLES_FOLDER = "articles/"
   
@@ -8,5 +10,18 @@ class ArticleFilesystem
       file.write(article.author + "|" + article.title + "|" + article.content)
       file.write("|" + article.likes.to_s + "|" << article.dislikes.to_s)
     end
+  end
+  
+  def self.read_from_file(path)
+    file = File.open(path, 'r')
+    line = file.first.split("|")
+    file.close
+    
+    art = Article.new(line[1], line[2], line[0])
+    likes = line[3].to_i
+    dislikes = line[4].to_i
+    likes.times { art.like! }
+    dislikes.times { art.dislike! }
+    art
   end
 end
